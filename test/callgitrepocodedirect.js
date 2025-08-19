88edc6a5216393569f0703ac98c9a453f0680e08
Steven Ossont <Steven.Ossont@Bizdata.co.uk>
2025-08-19 16:13:23 +1200

    test1

---
+++ test/callgitrepocodedirect.js
@@ -0,0 +1,4 @@
+// load this url and run it as a script in my page https://trustedresearchenvironment.github.io/PublicPortalTemplates/test/example1.js
+const script = document.createElement('script');
+script.src = 'https://trustedresearchenvironment.github.io/PublicPortalTemplates/test/example1.js';
+document.head.appendChild(script);
